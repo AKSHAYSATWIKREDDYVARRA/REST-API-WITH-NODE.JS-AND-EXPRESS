7e8dc1aa5b9104afefe5f89c4b3258a398e7e203
Akshay <2100031683cseh@gmail.com>
2024-11-11 16:29:46 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParses from 'bodyParser'; //it is used to turn the request from user into the readable format  
+import bodyParses from 'body-parser'; //it is used to turn the request from user into the readable format  
 
 
 const app = express(); // initializing the function 
@@ -7,5 +7,7 @@ const PORT = 9296; // here we creating an unique port number for the back end an
 
 app.use(bodyParses.json()); // here we are telling the express to use the body-parser only the json files or format
  
-app.listen(PORT, () => console.log(`Server running on the port: http://localhost${PORT}`)); // this function is used to listen the request from the user
+app.listen(PORT, () => console.log(`Server running on the port: http://localhost:${PORT}`)); // this function is used to listen the request from the user
 
+
+//NODEMON --> it is used to auto update the changes in the server with closing the server and run again the server for seeing the changes
